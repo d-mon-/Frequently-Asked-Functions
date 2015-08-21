8c3b2f2d3e3ac01a8684f65f196fb9e098dde32a
olivier <guerin.olivier@outlook.com>
2015-08-21 19:30:25 +0200

    add hex2rgb rgb2hex

---
+++ hex2rgb.js
@@ -0,0 +1,43 @@
+/**
+ * Created by GUERIN Olivier, on 21/08/2015.
+ * Twitter: @MisterRaton
+ */
+
+/**
+ * convert hexadecimal colors (FFFFFF) into rgb
+ * @param {String} v
+ * @returns {{r: Number, g: Number, b: Number}|Boolean}
+ */
+function hex2RGB (v){
+    if((/^#/i).test(v)){
+        v=v.slice(1);
+    }
+    var colorInt = parseInt(v,16);
+    if(colorInt>=0&&colorInt<=16777215){
+        colorInt+=16777216;
+        var rgb = (colorInt.toString(2)).slice(1).match(/.{1,8}/g);
+        return {r:parseInt(rgb[0],2),g:parseInt(rgb[1],2),b:parseInt(rgb[2],2)};
+    }
+    return false;
+}
+
+/**
+ *
+ * convert hexadecimal colors (FFFFFFFF) into rgb
+ * @param {String} v
+ * @returns {{a:Number, r: Number, g: Number, b: Number}|Boolean}
+ */
+function hex2RGBA (v){
+    if((/^#/i).test(v)){
+        v=v.slice(1);
+    }
+    var colorInt = parseInt(v,16);
+    if(colorInt>=0&&colorInt<=4294967295){
+        colorInt+=4294967296;
+        var rgb = (colorInt.toString(2)).slice(1).match(/.{1,8}/g);
+        return {a:parseInt(rgb[0],2),r:parseInt(rgb[1],2),g:parseInt(rgb[2],2),b:parseInt(rgb[3],2)};
+    }
+    return false;
+}
+hex2RGB("00ff00");
+hex2RGBA("00ffff00");
---
+++ rgb2hex.js
@@ -0,0 +1,44 @@
+/**
+ * Created by GUERIN Olivier, on 21/08/2015.
+ * Twitter: @MisterRaton
+ */
+/**
+ * isInteger and value between [0,255]
+ * @param {Number} v
+ * @returns {boolean}
+ */
+function isValidColorInteger(v){
+    return typeof v === 'number' && v%1 === 0 && v >= 0 && v <= 255;
+}
+
+/**
+ * convert r,g,b value into hexadecimal color
+ * @param {Number} r
+ * @param {Number} g
+ * @param {Number} b
+ * @returns {*}
+ */
+function rgb2hex (r,g,b){
+    if(isValidColorInteger(r)&&isValidColorInteger(g)&&isValidColorInteger(b)){
+        return(16777216+(r<<16)+(g<<8)+b).toString(16).slice(1)
+    }else{
+        return false;
+    }
+}
+/**
+ * convert r,g,b,a value into hexadecimal color
+ * @param {Number} r
+ * @param {Number} g
+ * @param {Number} b
+ * @param {Number} a
+ * @returns {*}
+ */
+function rgba2hex (r,g,b,a){
+    if(isValidColorInteger(a)&&isValidColorInteger(r)&&isValidColorInteger(g)&&isValidColorInteger(b)){
+        return(4294967296+((a<<24)>>>0)+(r<<16)+(g<<8)+b).toString(16).slice(1)
+    }else{
+        return false;
+    }
+}
+rgb2hex(0,255,255);
+rgba2hex(255,255,255,0);
