08539d89e68a2e04d9648b3ae0c34cebc57699cb
olivier <guerin.olivier@outlook.com>
2015-08-22 11:51:34 +0200

    algorithm modification

--- hex2rgb.js
+++ hex2rgb.js
@@ -2,6 +2,10 @@
  * Created by GUERIN Olivier, on 21/08/2015.
  * Twitter: @MisterRaton
  */
+/**
+ * Created by GUERIN Olivier, on 21/08/2015.
+ * Twitter: @MisterRaton
+ */
 
 /**
  * convert hexadecimal colors (FFFFFF) into rgb *
@@ -9,34 +13,28 @@
  * @returns {{r: Number, g: Number, b: Number}|Boolean}
  */
 function hex2RGB (v){
-    if((/^#/i).test(v)){
-        v=v.slice(1);
-    }
-    var colorInt = parseInt(v,16);
-    //colorInt>=000000/16 && colorInt <= ffffff/16
-    if(colorInt>=0&&colorInt<=16777215){
-        var rgb = ((colorInt+16777216).toString(2)).slice(1).match(/.{1,8}/g);
-        return {r:parseInt(rgb[0],2),g:parseInt(rgb[1],2),b:parseInt(rgb[2],2)};
+    if(typeof v==='string' && v.length===6){
+        var rgb = v.match(/[0-9a-f]{2}/gi);
+        if(rgb.length===3){
+            return {r:parseInt(rgb[0],16),g:parseInt(rgb[1],16),b:parseInt(rgb[2],16)};
+        }
     }
     return false;
 }
-
 /**
- *
  * convert hexadecimal colors (FFFFFFFF) into rgb
  * @param {String} v
  * @returns {{a:Number, r: Number, g: Number, b: Number}|Boolean}
  */
 function hex2RGBA (v){
-    if((/^#/i).test(v)){
-        v=v.slice(1);
-    }
-    var colorInt = parseInt(v,16);
-    if(colorInt>=0&&colorInt<=4294967295){
-        var rgb = ((colorInt+4294967296).toString(2)).slice(1).match(/.{1,8}/g);
-        return {a:parseInt(rgb[0],2),r:parseInt(rgb[1],2),g:parseInt(rgb[2],2),b:parseInt(rgb[3],2)};
+    if(typeof v==='string' && v.length===8){
+        var rgb = v.match(/[0-9a-f]{2}/gi);
+        if(rgb.length===4){
+            return {a:parseInt(rgb[0],16),r:parseInt(rgb[1],16),g:parseInt(rgb[2],16),b:parseInt(rgb[3],16)};
+        }
     }
     return false;
 }
+
 hex2RGB("00ff00");
 hex2RGBA("00ffff00");
