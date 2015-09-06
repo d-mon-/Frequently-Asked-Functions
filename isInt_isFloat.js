3fc65067d62c5788d8d9ca0e32b60edc7ebf2c39
guerin olivier <oli.grayrabbit@gmail.com>
2015-09-06 22:12:30 +0200

    remove test

--- isInt_isFloat.js
+++ isInt_isFloat.js
@@ -13,17 +13,3 @@ function isFloat(v){
 function isFloat2(v){
     return typeof v ==='number' && !isNaN(v) && isFinite(v);
 }
-
-function tester(){
-    var f = Object.prototype.toString;
-    var x = [1,-1,1.1,0,-Infinity,Infinity,NaN,null,undefined,"1","test",{},[1],function(){ return null},function(){ return 1;},"",[]];
-    var y = [isInt,isFloat,isFloat2];
-
-    y.forEach(function(elem,index,array){
-        console.log("--------"+elem.name+"-------");
-        x.forEach(function(xelem,xindex,xarray){
-            console.log(f.call(xarray[xindex])+":"+xarray[xindex]+"==>"+elem(xelem));
-        });
-    });
-}
-tester();
