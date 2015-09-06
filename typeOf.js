5eebb4111f6fd48ebbf839724e939fc88a488e5b
olivier <guerin.olivier@outlook.com>
2015-09-06 23:17:08 +0200

    add typeOf function

---
+++ typeOf.js
@@ -0,0 +1,17 @@
+/**
+ * Created by GUERIN Olivier, on 06/09/2015.
+ * Twitter: @MisterRaton
+ */
+function typeOf(value){
+    if(typeof value === 'object' && value!==null){
+        if(value.constructor.name!==undefined){
+            return value.constructor.name;
+        }else{
+            return (value.constructor.toString()).match(/^function\s+([\w\$]+)\s*\(/)[1];
+        }
+    }
+    if(typeof value ==="number" && isNaN(value)){
+        return "NaN";
+    }
+    return Object.prototype.toString.call(value).slice(8,-1);
+}
